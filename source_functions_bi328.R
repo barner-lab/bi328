10bbd882e64f67b006945d23a53dbe00b84989b0
allison barner <allisonkbarner@gmail.com>
2021-02-14 14:57:46 -0500

    upload function for class packages

---
+++ source_functions_bi328.R
@@ -0,0 +1,10 @@
+## functions for BI328 lab
+
+#### load packages ####
+
+load_bi328 <- function() {
+  library(dplyr); library(magrittr); library(tidyr); library(purrr); library(stringr); library(tibble); library(readr); library(vegan); library(ggplot2)
+}
+
+
+
